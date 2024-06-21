392204bc3514d89d0fcbacfe1fd3f5835e65b6fe
thoophoms <thoophom.su@gmail.com>
2024-06-21 18:24:40 -0400

    HelloWorld is created

---
+++ src/HelloWorld/Main.java
@@ -0,0 +1,11 @@
+package HelloWorld;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("Hello, world");
+
+        System.out.println();
+        System.out.println("Use System.out.println to print another line");
+    }
+}
